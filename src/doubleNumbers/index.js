b983872386d8ee5d0a8185e8d0ab791d389bb236
Dmytro Kharchenko <30443685+dkharch@users.noreply.github.com>
2018-10-30 11:59:09 +0200

    Code refactoring

--- src/doubleNumbers/index.js
+++ src/doubleNumbers/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable-next-line */
 export const doubleNum = num => {
   const numStr = num.toString();
-  const firstPart = numStr.slice(0,numStr.length / 2);
+  const firstPart = numStr.slice(0, numStr.length / 2);
   const secondPart = numStr.slice(numStr.length / 2);
   return firstPart === secondPart ? num : num * 2;
 };
